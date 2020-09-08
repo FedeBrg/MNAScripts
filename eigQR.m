b5a2b6f3283dd8e3d74c140961116399783b2ffb
FedeBrg <fede.bergagna@gmail.com>
2020-09-08 13:11:14 -0300

    Autovalores con QR

---
+++ eigQR.m
@@ -0,0 +1,21 @@
+function v = eigQR(A,n,error)
+  
+  numiter=n;
+  eps=error;
+  Ak = A;
+  
+ 
+  for k=1:numiter
+    [Q R] = qr(Ak);
+    Ak1 = R*Q;
+    v = diag(Ak1);
+
+    if abs(diag(Ak)-diag(Ak1))<eps
+      return
+  end
+  Ak = Ak1;
+  
+  end
+return
+  
+endfunction
