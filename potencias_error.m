7abd6e3317d0b590eaeff728859ec2485406be5e
FedeBrg <fede.bergagna@gmail.com>
2020-09-08 12:21:23 -0300

    Potencias con error

---
+++ potencias_error.m
@@ -0,0 +1,23 @@
+function [y,l]=potencias_error(a,v,n,error)
+% Metodo de las potencias
+% En cada paso se divide el vector que se obtiene por su norma
+% numiter es el numero m\’{a}ximo de iteraciones
+% eps es la precision fijada
+%
+% Si la diferencia entre dos valores consecutivos calculados de l
+% es menor que eps, el programa se detiene
+  numiter=n;
+  eps=error;
+  x=v(:);
+  y=x/norm(x);
+  l=0;
+  for k=1:numiter
+    x=a*y;
+    y=x/norm(x);
+    lc=x.’*y;
+    if abs(l-lc)<eps
+      return
+  end
+  l=lc;
+  end
+return
